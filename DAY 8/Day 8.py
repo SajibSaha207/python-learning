446a256e0d7ae95adb1b08e74e632e40701f320b
SajibSaha207 <sajibsaha.207@gmail.com>
2026-01-14 07:55:36 +0600

    second problem done

--- DAY 8/Day 8.py
+++ DAY 8/Day 8.py
@@ -1,10 +1,21 @@
 #1
-student = {
-    "name": "Sajib",
-    "age": 22,
-    "department": "CSE"
+# student = {
+#     "name": "Sajib",
+#     "age": 22,
+#     "department": "CSE"
+# }
+# print(student["name"])
+# student["age"]= 23
+# print(student["age"])
+# print(student)
+
+#2
+car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2020
 }
-print(student["name"])
-student["age"]= 23
-print(student["age"])
-print(student)
+car["color"]= "white"
+print(car)
+car.pop("year")
+print(car)
