34e397dbd07e5981e2c375b9275595d60bfe18ff
SajibSaha207 <sajibsaha.207@gmail.com>
2026-01-14 11:10:56 +0600

    2nd mix problem done

--- DAY 8/Mix problem.py
+++ DAY 8/Mix problem.py
@@ -1,15 +1,25 @@
 #1
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
+# num1 = int(input("Enter your first number: "))
+# num2 = int(input("Enter your second number: "))
 
-total_sum = num1 + num2
-print(total_sum)
+# total_sum = num1 + num2
+# print(total_sum)
 
-total_diff = num1 - num2
-print(total_diff)
+# total_diff = num1 - num2
+# print(total_diff)
 
-total_mul = num1 * num2
-print(total_mul)
+# total_mul = num1 * num2
+# print(total_mul)
 
-total_div = num1 / num2
-print(total_div)
+# total_div = num1 / num2
+# print(total_div)
+
+#2
+num = int(input("Enter your  number: "))
+
+if num >= 0:
+    print("Positive")
+elif num <= 0:
+    print("Negative")
+else:
+    print("Number is zero")
