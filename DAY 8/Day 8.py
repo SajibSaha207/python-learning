7873466b3ff2912ec672a8543ad420171d796b72
SajibSaha207 <sajibsaha.207@gmail.com>
2026-01-14 08:07:09 +0600

    3rd problem done

--- DAY 8/Day 8.py
+++ DAY 8/Day 8.py
@@ -10,12 +10,30 @@
 # print(student)
 
 #2
-car = {
-    "brand": "Toyota",
-    "model": "Corolla",
-    "year": 2020
+# car = {
+#     "brand": "Toyota",
+#     "model": "Corolla",
+#     "year": 2020
+# }
+# car["color"]= "white"
+# print(car)
+# car.pop("year")
+# print(car)
+
+#3
+
+marks = {
+    "Math": 85,
+    "English": 78,
+    "Physics": 92
 }
-car["color"]= "white"
-print(car)
-car.pop("year")
-print(car)
+
+for sub in marks:
+    print(sub)
+for mark in marks.values():
+    print(mark)
+
+total_sum = 0
+# for mark in marks:
+total_sum += mark
+print(f"Total_sum: {total_sum}")
