a677db2ac52b3c380d4ddc4b226171082bd526ed
SajibSaha207 <sajibsaha.207@gmail.com>
2026-01-14 09:19:44 +0600

    1st mix problem done

---
+++ DAY 8/Mix problem.py
@@ -0,0 +1,15 @@
+#1
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+total_sum = num1 + num2
+print(total_sum)
+
+total_diff = num1 - num2
+print(total_diff)
+
+total_mul = num1 * num2
+print(total_mul)
+
+total_div = num1 / num2
+print(total_div)
