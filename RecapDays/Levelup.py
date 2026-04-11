0ed51f88d8ac834406aa98bc94aa1ea76b7d1ae3
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-11 19:37:52 +0600

    levelup problem done

---
+++ RecapDays/Levelup.py
@@ -0,0 +1,17 @@
+#1
+# from collections import Counter
+# numbers = [1, 2, 2, 3, 3, 4]
+
+# count = Counter(numbers)
+# res =[num for num, frq in count.items() if frq > 1 ]
+# print(res)
+
+#2
+# numbers = [10, 25, 5, 30]
+# smallest=10
+# for num in numbers:
+#     if num<smallest:
+#         smallest=num
+# print(smallest)
+
+
