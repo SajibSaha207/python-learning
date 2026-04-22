7ec8c0c510883ad15fede17792488b33df8d28eb
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-22 09:39:11 +0600

    Levelup day 2 2nd problem sloved

--- RecapDays/LevelUpDay2.py
+++ RecapDays/LevelUpDay2.py
@@ -1,7 +1,24 @@
 #1 Count how many numbers biger than 0
 
-numbers = [-2, 5, 0, 3, -1, 8]
+# 
+# numbers = [-2, 5, 0, 3, -1, 8]
+# count = 0
 
-for num in numbers:
-    if num > 0:
-        print(num)
+# for num in numbers:
+#     if num > 0:
+#      count += 1
+# print(count)
+
+#2 count vowel
+text = "education"
+vowel = "aeiouAEIOU"
+count = 0
+
+for char in text:
+    if char in vowel:
+        count += 1
+print(count)
+        
+
+
+
