5ada735583031d8bb0975323abcb28a1c4633e5d
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-22 09:00:59 +0600

    Levelup day 2 1st problem sloved

---
+++ RecapDays/LevelUpDay2.py
@@ -0,0 +1,7 @@
+#1 Count how many numbers biger than 0
+
+numbers = [-2, 5, 0, 3, -1, 8]
+
+for num in numbers:
+    if num > 0:
+        print(num)
--- RecapDays/Levelup.py
+++ RecapDays/Levelup.py
@@ -17,21 +17,23 @@
 
 
 #
-numbers = [1, 2, 2, 3, 3, 4]
+# numbers = [1, 2, 2, 3, 3, 4]
 
-for i in range(len(numbers)):
-    count = 0
-    for j in range(len(numbers)):
-         if numbers [i] == numbers [j]:
-              count += 1
+# for i in range(len(numbers)):
+#     count = 0
+#     for j in range(len(numbers)):
+#          if numbers [i] == numbers [j]:
+#               count += 1
     
-    if count > 1:
-         print(numbers[i])
+#     if count > 1:
+#          print(numbers[i])
 
-# for j in range(len(numbers)):
-#     if numbers[i] == numbers[j]:
-#           count += 1
 
-# if count > 1:
-#     print(numbers[i])
+# nums = [2, 7, 11, 15]
+# target = 9
+
+# for i in range(len(nums)):
+#     for j in range(i + 1, len(nums)):
+#         if nums [i] + nums [j] == target:
+#             print(i,j)
 
